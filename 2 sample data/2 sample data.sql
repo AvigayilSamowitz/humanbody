7f91a187f625d8d4feb8036388666dcc031d0bcc
AvigayilSamowitz <avigayilsam@gmail.com>
2024-02-11 12:03:22 -0800

    code review see comments in files

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,8 +1,9 @@
+-- AS please populate sample data with planets to match your new schema
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouuth', 2
+union select 'mouuth', 2 -- AS was this change unintentional
 union select 'teeth', 32
 union select 'toungue', 1
 
